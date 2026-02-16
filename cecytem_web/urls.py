3b14215c596c5c26029c79f922ea742d9e8bd431
Your Name <macielbrayn@gmail.com>
2026-02-15 23:54:18 -0600

    urls modificada

--- cecytem_web/urls.py
+++ cecytem_web/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from asistencia import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('asistencia/', views.lista_asistencia, name='lista_asistencia'),
-    path("descargar/", views.descargar_excel, name="descargar_excel"),
-    path("json/", views.lista_asistencia_json, name="lista_asistencia_json"),
+    path("admin/", admin.site.urls),
     path("alumnos/", include("alumnos.urls")),
     path("usuarios/", include("usuarios.urls")),
     path("reportes/", include("reportes.urls")),
     path("administracion/", include("administracion.urls")),
+    path("asistencia/", include("asistencia.urls")),
 ]
